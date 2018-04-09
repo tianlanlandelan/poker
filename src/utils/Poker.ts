ce745df0307f65c6c05e3127291bec624cc684f1
yangkaile <guyexing@foxmail.com>
2018-04-09 19:59:19 +0800

    同步代码

--- src/utils/Poker.ts
+++ src/utils/Poker.ts
@@ -1,8 +1,20 @@
 class Poker {
-	public constructor(id:number,orderValue:string) {
+	public constructor(id:number,orderString:string) {
         this.id = id;
-        this.orderValue = orderValue;
+        this.orderString = orderString;
+        this.sort = PukerTypeUtils.orderString.indexOf(orderString)
     }
+
+    /**
+     * 牌面ID
+     */
     private id:number;
-    private orderValue:string;
+    /**
+     * 牌大小字符表示
+     */
+    private orderString:string;
+    /**
+     * 牌大小排序值 0-14  数值越小，表示牌越大
+     */
+    private sort:number;
 }
